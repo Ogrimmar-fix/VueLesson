61395e4119314b26aa83038ee6ff399a55b7fac1
EinLied <megopixel91@gmail.com>
2025-06-16 21:50:15 +0300

    Fix bug

--- GitProject/src/App.vue
+++ GitProject/src/App.vue
@@ -1,20 +1,75 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script lang="ts">
+import MyEmployee from './components/MyEmployee.vue'
+
+export default {
+  components: {
+    MyEmployee,
+  },
+  data() {
+    return {
+      currentId: 2,
+      nameBox: ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'],
+      rowsDelo: [
+        {
+          id: 1,
+          name: 'DeloOne',
+          isRemoved: true,
+        },
+        {
+          id: 2,
+          name: 'DeloTwo',
+          isRemoved: false,
+        },
+      ],
+    }
+  },
+  methods: {
+    crossOut(id: number, value: boolean) {
+      const element = this.rowsDelo.find(x => x.id === id);
+      if (!element) {
+        return;
+      }
+
+      element.isRemoved = value;
+      console.log(this.rowsDelo, 'this.rowsDelo');
+    },
+    remove(id: number) {
+      this.rowsDelo = this.rowsDelo.filter((elem) => {
+        return elem.id !== id
+      })
+    },
+    addLine() {
+      const newIndex = this.currentId++;
+      let newName = 'Delo ';
+      const numberStringValue = this.nameBox[newIndex];
+      console.log(newIndex, 'newIndex');
+      newName += numberStringValue ? numberStringValue : newIndex;
+
+      this.rowsDelo.push({
+        id: newIndex,
+        name: newName,
+        isRemoved: false,
+      })
+    },
+  },
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <MyEmployee
+    class="parent"
+    v-for="elem in rowsDelo"
+    :key="elem.id"
+    :element="elem.name"
+    :id="elem.id"
+    :isChecked="elem.isRemoved"
+    @remove="remove"
+    @cross-out="crossOut"
+  />
+  <button @click="addLine">Добавить</button>
 </template>
-
-<style scoped>
+<style>
+.parent {
+  margin: auto;
+}
 </style>
